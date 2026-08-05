1a7aa27ef3e879db6bd88a9e5592ea8f23139894
Diego Cano <57439165+dfc-coder@users.noreply.github.com>
2026-08-05 16:58:26 -0300

    chore: load final interaction tuning

--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,6 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import "./style.css";
 import "./cinematic-refined.css";
+import "./cinematic-tuning.css";
 
 createApp(App).mount("#app");
