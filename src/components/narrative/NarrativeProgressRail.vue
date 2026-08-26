e1f54623422cf72d1f74023ef99c1f9d149140c1
Diego Cano <57439165+dfc-coder@users.noreply.github.com>
2026-08-26 20:55:40 -0300

    Add shared narrative progress rail

---
+++ src/components/narrative/NarrativeProgressRail.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+type RailItem = {
+  key: string;
+  label: string;
+  meta?: string;
+};
+
+defineProps<{
+  items: RailItem[];
+}>();
+</script>
+
+<template>
+  <div class="narrative-progress-rail narrative-rail" aria-hidden="true">
+    <i class="narrative-progress-rail__progress" />
+    <div class="narrative-progress-rail__items">
+      <div
+        v-for="(item, index) in items"
+        :key="item.key"
+        class="narrative-progress-rail__item"
+        :data-index="index"
+        :style="{ '--rail-slot': items.length > 1 ? index / (items.length - 1) : 0 }"
+      >
+        <i />
+        <b>{{ item.label }}</b>
+        <span v-if="item.meta">{{ item.meta }}</span>
+      </div>
+    </div>
+  </div>
+</template>
